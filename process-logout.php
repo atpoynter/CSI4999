643ce2f7603db7186e6205c01f0a7fbda3a6b42b
yousifsallan <65097640+yousifsallan@users.noreply.github.com>
2021-02-16 11:09:34 -0500

    Delete process-logout.php

--- process-logout.php
+++
@@ -1,9 +0,0 @@
-<!-- this destroys my connection and session -->
-<?php
-session_start();
-session_destroy();
-?>
-<!-- Quick redirect to previous page -->
-<script type="text/javascript">
-    window.location = "javascript:history.go(-1)";
-</script>
