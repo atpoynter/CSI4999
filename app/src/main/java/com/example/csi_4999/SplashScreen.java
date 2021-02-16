bfa98843ffcec9a300bb7201e74b54d7fad1c66b
atpoynter <55966011+atpoynter@users.noreply.github.com>
2021-02-16 11:14:35 -0500

    1st Build
    Black Activities

---
+++ app/src/main/java/com/example/csi_4999/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.example.csi_4999;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //ImageView img = (ImageView)findViewById(R.id.imageView3);
+
+        Thread timerThread = new Thread(){
+
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,WebView.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
