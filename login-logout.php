7b768f668bd5543db65402f8fa99199cf8b75108
yousifsallan <65097640+yousifsallan@users.noreply.github.com>
2021-02-16 11:08:49 -0500

    Delete login-logout.php

--- login-logout.php
+++
@@ -1,9 +0,0 @@
-<!-- code that uses includes to display proper login elements via php includes -->
-<?php
-  if ($_SESSION['logged_in'] == TRUE) {
-    include("logout-element.html");
-  } else {
-    include("login-element.html");
-  }
-
-?>
